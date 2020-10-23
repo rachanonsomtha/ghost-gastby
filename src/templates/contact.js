7a27501160d891dd208d0ae21eead555e03dcbfa
rachanonsomtha <rachanon.som@gmail.com>
2020-10-23 10:52:46 +0700

    create new static page

---
+++ src/templates/contact.js
@@ -0,0 +1,16 @@
+import { Layout } from '../components/common'
+
+const Contact = () => (
+    <Layout>
+        <div className="container">
+            <article className="content" style={{ textAlign: `center` }}>
+                <h1 className="content-title">Test</h1>
+                <section className="content-body">
+                    Test page.
+                </section>
+            </article>
+        </div>
+    </Layout>
+)
+
+export default Contact
